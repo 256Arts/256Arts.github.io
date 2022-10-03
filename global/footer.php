e81ed822fdca8360a99b997860b9cd70719edde6
256ArtsDeveloper <256arts@jaydenirwin.com>
2022-10-03 16:48:17 -0400

    first

---
+++ global/footer.php
@@ -0,0 +1,9 @@
+		<footer>
+			<section class="content">
+				<p class="minor">Copyright &#169; 2017 Jayden Irwin. All rights reserved.</p>
+				<a href="/contact/">Contact</a>
+	            <a href="http://twitter.com/jaydenirwin/" target="_blank">Twitter</a>
+	        </section>
+	    </footer>
+	</body>
+</html>
